85ea0804b2ca1effb1ed4aaac884e41d67cb51a1
peeteko <koen.peeters@bpost.be>
2024-09-13 08:20:40 +0200

    initial version

---
+++ src/test/java/com/example/docker/JavaAppOnDocker/JavaAppOnDockerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.docker.JavaAppOnDocker;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class JavaAppOnDockerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
